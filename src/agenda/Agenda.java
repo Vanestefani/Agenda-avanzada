fd2a38df617898ac1892559b073f26db393bfb51
Vanessa Estefania Corredor Andrade <estanikisu@gmail.com>
2019-01-27 17:15:05 -0500

    instanciando loading en la clase main

--- src/agenda/Agenda.java
+++ src/agenda/Agenda.java
@@ -5,6 +5,8 @@
  */
 package agenda;
 
+import Vista.Loading;
+
 /**
  *
  * @author Vanessa Estefania Corredor Andrade
@@ -15,7 +17,7 @@ public class Agenda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      Loading splash = new Loading();
     }
     
 }
